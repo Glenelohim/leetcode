167aa3eebabbcf2fafed40299d13d2ac60cc109c
glen_mac <zkjxlh@163.com>
2015-12-01 20:16:46 +0800

    ac NO.10

--- regular_expression_matching_10.cpp
+++ regular_expression_matching_10.cpp
@@ -1,11 +1,22 @@
 class Solution {
 public:
-    bool isMatch(string s, string p) {
-        int s_len = s.length();
-        int p_len = p.length();
-        if (!s_len && !p_len) return true;
-        if (!s_len || !p_len) return false;
-
-        
+    bool isMatch(char* s, char* p) {
+    if (*p == '\0') return *s == '\0';
+    if (*(p+1) == '*')
+    {
+        while(*p == *s || (*p == '.' && *s != '\0'))
+        {
+            if (isMatch(s, p+2))
+                return true;
+            s++;
+        }
+        return isMatch(s, p+2);
     }
+    else
+    {
+        if (*p == *s || (*p == '.' && *s != '\0'))
+            return isMatch(s+1, p+1);
+        return false;
+    }
+}
 };
