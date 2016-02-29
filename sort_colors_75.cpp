89a315655e46d7feb5702a38ecc8960f9d8757b0
glen_mac <zkjxlh@163.com>
2016-02-29 22:30:44 +0800

    ac 2016.02.29

--- sort_colors_75.cpp
+++ sort_colors_75.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
-	void quicksort(vector<int>& nums, int low, int high)
-	{
-		int key = nums[low];
-		int i = low, j = high;
-		while (i <= j)
-		{
-			
-		}
-	}
     void sortColors(vector<int>& nums) {
-        
+        int nums_size = nums.size();
+        if (!nums_size)
+        	return;
+        int num0 = 0, num1 = 0, num2 = 0;
+        for (int i = 0; i < nums_size; i++)
+        {
+        	if (nums[i] == 0)
+        		num0++;
+        	else if (nums[i] == 1)
+        		num1++;
+        	else
+        		num2++;
+        }
+        for (int i = 0; i < nums_size; i++)
+        {
+        	if (i < num0)
+        		nums[i] = 0;
+        	else if (i < (num0 + num1))
+        		nums[i] = 1;
+        	else
+        		nums[i] = 2;
+        }
     }
 };
