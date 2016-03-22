329f6b3a90dcdd2c42553f89cdae12c956e4de3c
glen <zkjxlh@163.com>
2016-03-22 11:07:05 +0800

    ac 20160322

--- insertion_sort_list_147.cpp
+++ insertion_sort_list_147.cpp
@@ -13,6 +13,23 @@ public:
         {
         	return head;
         }
-        ListNode* 
+        ListNode* dummy = new ListNode(0);
+        ListNode* p = head;
+        ListNode* cur = dummy;
+        ListNode* q = NULL;
+        while (p)
+        {
+        	cur = dummy;
+        	while (cur->next && cur->next->val < p->val)
+        	{
+        		cur = cur->next;
+        	}
+        	q = p->next;
+        	p->next = cur->next;
+        	cur->next = p;
+        	p = q;
+        }
+
+        return dummy->next;
     }
 };
